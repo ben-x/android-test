97ce9892a817b7d11eca365207336d44b48a90f1
Paul-wiseman <paulndibe92@gmail.com>
2021-07-14 13:07:00 +0100

    Added location tracking and a function to calculate the total distance covered

--- app/src/main/java/ng/riby/androidtest/viewmodel/UserLocationViewModel.java
+++ app/src/main/java/ng/riby/androidtest/viewmodel/UserLocationViewModel.java
@@ -7,22 +7,22 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
-import ng.riby.androidtest.database.entities.Location;
+import ng.riby.androidtest.database.entities.UserLocation;
 import ng.riby.androidtest.repository.LocationRepository;
 
 public class LocationViewModel extends AndroidViewModel {
 
     private LocationRepository locationRepository;
 
-    private final LiveData<List<Location>> getlocations;
+    private final LiveData<List<UserLocation>> getlocations;
 
-    public LocationViewModel(Application application, LiveData<List<Location>> getlocations) {
+    public LocationViewModel(Application application) {
         super(application);
         locationRepository = new LocationRepository(application);
         this.getlocations = locationRepository.getLocation();
     }
 
-    LiveData<List<Location>> getAllLocations() { return getlocations; }
+    LiveData<List<UserLocation>> getAllLocations() { return getlocations; }
 
-    public void insert(Location location) { locationRepository.insert(location); }
+    public void insert(UserLocation userLocation) { locationRepository.insert(userLocation); }
 }
